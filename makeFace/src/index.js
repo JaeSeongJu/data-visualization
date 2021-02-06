9b344cfd05b5ff0ef44fea37d93d8338d7c6135b
JooJaeSeong <joojae26@gmail.com>
2021-02-06 18:37:57 +0900

    Compartmentalizing Complexity by refactoring & experience strength of React

--- makeFace/src/index.js
+++ makeFace/src/index.js
@@ -1,36 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { arc } from "d3";
-import { BackgroundCircle } from "./BackgroundCircle";
-import { Eyes } from "./Eyes";
-import { Mouth } from "./Mouth";
-const width = 960;
-const height = 500;
-const centerX = width / 2;
-const centerY = height / 2;
-const strokeWidth = 20;
-const eyeOffsetX = 90;
-const eyeOffsetY = 100;
-const eyeRadius = 40;
-const mouthWidth = 20;
-const mouthRadius = 160;
+import { range } from "d3";
+import { Face } from "./Face";
 
-const App = () => (
-  <svg width={width} height={height}>
-    <g transform={`translate(${centerX},${centerY})`}>
-      <BackgroundCircle
-        radius={centerY - strokeWidth / 2}
-        strokeWidth={strokeWidth}
-      />
-      <Eyes
-        eyeOffsetX={eyeOffsetX}
-        eyeOffsetY={eyeOffsetY}
-        eyeRadius={eyeRadius}
-      />
-      <Mouth mouthRadius={mouthRadius} mouthWidth={mouthWidth} />
-    </g>
-  </svg>
-);
+const width = 150;
+const height = 150;
+// reduce variables
+
+const array = range(10 * 5);
+
+const App = () =>
+  array.map(() => (
+    <Face
+      width={width}
+      height={height}
+      centerX={width / 2}
+      centerY={height / 2}
+      strokeWidth={10 + Math.random() * 5}
+      eyeOffsetX={30 + Math.random() * 10}
+      eyeOffsetY={30 + Math.random() * 10}
+      eyeRadius={10 + Math.random() * 5}
+      mouthWidth={10 + +Math.random() * 5}
+      mouthRadius={40 + Math.random() * 5}
+    />
+  ));
+// symentically meaningful JSX refactoring
+// process of refactoring components & modules allows complexity to scale
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
