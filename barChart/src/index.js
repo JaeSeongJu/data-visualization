7e424bd53f2a322821e3b4f062b63c64acd1509d
JooJaeSeong <joojae26@gmail.com>
2021-02-15 22:47:57 +0900

    Add axes using 'ticks'

--- barChart/src/index.js
+++ barChart/src/index.js
@@ -7,7 +7,7 @@ const csvUrl =
 
 const width = 960;
 const height = 500;
-const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+const margin = { top: 20, right: 20, bottom: 20, left: 200 };
 
 const App = () => {
   const [data, setData] = useState(null);
@@ -40,8 +40,32 @@ const App = () => {
   return (
     <svg width={width} height={height}>
       <g transform={`translate(${margin.left},${margin.top})`}>
+        {xScale.ticks().map((tickValue) => (
+          <g key={tickValue} transform={`translate(${xScale(tickValue)},0)`}>
+            <line y2={innerHeight} stroke="black" />
+            <text
+              style={{ textAnchor: "middle" }}
+              dy=".71em"
+              y={innerHeight + 3}
+            >
+              {tickValue}
+            </text>
+          </g>
+        ))}
+        {yScale.domain().map((tickValue) => (
+          <text
+            key={tickValue}
+            style={{ textAnchor: "end" }}
+            dy=".32em"
+            x="-3"
+            y={yScale(tickValue) + yScale.bandwidth() / 2}
+          >
+            {tickValue}
+          </text>
+        ))}
         {data.map((d) => (
           <rect
+            key={d.Country}
             x={0}
             y={yScale(d.Country)}
             width={xScale(d.Population)}
